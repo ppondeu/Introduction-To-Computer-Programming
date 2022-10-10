6534f4ef9791d5af78399c769c4f6959d1793e66
ppondeu <106013516+ppondeu@users.noreply.github.com>
2022-10-10 17:04:45 +0700

    Update code.ino

--- Assingment5_FSM/code.ino
+++ Assingment5_FSM/code.ino
@@ -1,12 +1,12 @@
-#define LED_S_R 5
-#define LED_S_Y 6
-#define LED_S_G 7
-#define BUTTON_S 2
+#define LED_S_R 8
+#define LED_S_Y 9
+#define LED_S_G 10
+#define BUTTON_S 3
 
-#define LED_WT_R 8
-#define LED_WT_Y 9
-#define LED_WT_G 10
-#define BUTTON_WT 3
+#define LED_WT_R 5
+#define LED_WT_Y 6
+#define LED_WT_G 7
+#define BUTTON_WT 2
 
 #define LED_WK_G 11
 #define LED_WK_R 12
@@ -28,63 +28,65 @@ int inputWk;
 
 struct State
 {
-	unsigned long ST_Out;
-	unsigned long Time;
-	unsigned long Next[8];
-} ;
-
-typedef const struct State SType;
-
-Stype FSM[7] = {
-	{B00110001, 3000, {goS, waitS, waitS, waitS, goS, waitS, waitS, waitS}},
-	{B01010001, 500, {goWt, goWt, goWt, goWt, goWt, goWt, goWt, goWt}},
-	{B10000101, 3000, {goWt, waitWt, goWt, waitWt, waitWt, waitWt, waitWt, waitWt}},
-	{B10001001, 300, {goWk, goWk, goWk, goWk, goWk, goWk, goWk, goWk}},
-	{B10010010, 200, {goWk, goWk, lightOff, lightOff, lightOff, lightOff, lightOff, lightOff}},
-	{B10010010, 100, {lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2}},
-	{B10010000, 100, {lightOn, lightOn, lightOn, lightOn, lightOn, lightOn, lightOn, lightOn}},
-	{B10010000, 100, {goS, goS, lightOn, goS, goS, goS, goS, goS}},
+  unsigned long ST_Out;
+  unsigned long Time;
+  unsigned long Next[8];
 };
 
-unsinged long S = 0;
+//typedef const struct State Stype;
+
+struct State FSM[8] = {
+                    //000 001    010    011    100   101    110    111 (S Wt Wk)
+  {B00110001, 2000, {goS, waitS, waitS, waitS, goS, waitS, waitS, waitS}}, //goS
+  {B01010001, 300, {goWk, goWk, goWt, goWt, goWk, goWk, goWt, goWt}}, //waitS
+  {B10000101, 2000, {goWt, waitWt, goWt, waitWt, waitWt, waitWt, waitWt, waitWt}}, //goWt
+  {B10001001, 300, {goWk, goWk, goWk, goWk, goS, goWk, goS, goWk}}, //waitWt
+  {B10010010, 2000, {goWk, goWk, lightOff, lightOff, lightOff, lightOff, lightOff, lightOff}}, //goWk
+  {B10010010, 200, {lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2, lightOff2}}, //lightOn
+  {B10010000, 100, {lightOn, lightOn, lightOn, lightOn, lightOn, lightOn, lightOn, lightOn}}, //lightOff
+  {B10010000, 100, {goS, goS, goWt, goWt, goS, goS, goS, goS}}, //lightOff2
+};
+
+unsigned long S = 0;
 
 void setup()
 {
-	pinMode(LED_S_R, OUTPUT);
-	pinMode(LED_S_Y, OUTPUT);
-	pinMode(LED_S_G, OUTPUT);
-	pinMode(BUTTON_S, INPUT_PULLUP);
+  pinMode(LED_S_R, OUTPUT);
+  pinMode(LED_S_Y, OUTPUT);
+  pinMode(LED_S_G, OUTPUT);
+  pinMode(BUTTON_S, INPUT_PULLUP);
 
-	pinMode(LED_WT_R, OUTPUT);
-	pinMode(LED_WT_Y, OUTPUT);
-	pinMode(LED_WT_G, OUTPUT);
-	pinMode(BUTTON_WT, INPUT_PULLUP);
+  pinMode(LED_WT_R, OUTPUT);
+  pinMode(LED_WT_Y, OUTPUT);
+  pinMode(LED_WT_G, OUTPUT);
+  pinMode(BUTTON_WT, INPUT_PULLUP);
 
-	pinMode(LED_WK_G, OUTPUT);
-	pinMode(LED_WK_R, OUTPUT);
-	pinMode(BUTTON_WK, INPUT_PULLUP);
+  pinMode(LED_WK_G, OUTPUT);
+  pinMode(LED_WK_R, OUTPUT);
+  pinMode(BUTTON_WK, INPUT_PULLUP);
 }
 
 void loop()
 {
 
-	digitalWrite(LED_S_R, FSM[S].ST_Out & B10000000);
-	digitalWrite(LED_S_Y, FSM[S].ST_Out & B01000000);
-	digitalWrite(LED_S_G, FSM[S].ST_Out & B00100000);
+  digitalWrite(LED_S_R, FSM[S].ST_Out & B10000000);
+  digitalWrite(LED_S_Y, FSM[S].ST_Out & B01000000);
+  digitalWrite(LED_S_G, FSM[S].ST_Out & B00100000);
 
-	digitalWrite(LED_WT_R, FSM[S].ST_Out & B00010000);
-	digitalWrite(LED_WT_Y, FSM[S].ST_Out & B00001000);
-	digitalWrite(LED_WT_G, FSM[S].ST_Out & B00000100);
+  digitalWrite(LED_WT_R, FSM[S].ST_Out & B00010000);
+  digitalWrite(LED_WT_Y, FSM[S].ST_Out & B00001000);
+  digitalWrite(LED_WT_G, FSM[S].ST_Out & B00000100);
 
-	digitalWrite(LED_WK_G, FSM[S].ST_Out & B0000010);
-	digitalWrite(LED_WK_R, FSM[S].ST_Out & B00000001);
+  digitalWrite(LED_WK_G, FSM[S].ST_Out & B00000010);
+  digitalWrite(LED_WK_R, FSM[S].ST_Out & B00000001);
 
-	delay(FSM[S].Time);
-
-	inputS = !digitalRead(BUTTON_S);
-	inputWt = !digitalRead(BUTTON_WT);
-	inputWk = !digitalRead(BUTTON_WK);
-
-	input = 4*inputS + 2*inputWt + inputWk;
-	S = FSM[S].Next[input];
+  delay(FSM[S].Time);
+  
+  inputS = !digitalRead(BUTTON_S);
+  inputWt = !digitalRead(BUTTON_WT);
+  inputWk = !digitalRead(BUTTON_WK);
+  
+  input = 4*inputS + 2*inputWt + inputWk;
+ 
+  S = FSM[S].Next[input];
 }
